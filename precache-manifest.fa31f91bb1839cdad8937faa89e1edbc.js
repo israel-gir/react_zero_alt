2a798c215249922f91c895a2e0511886f6e96503
israel-gir <israel@gir.co>
2020-04-24 15:36:01 -0500

    Updates

---
+++ precache-manifest.fa31f91bb1839cdad8937faa89e1edbc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74b5ea717e9ddad349adc3a87ec7593f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "086cfc8a2be0a85360d4",
+    "url": "/static/css/main.ddf3f760.chunk.css"
+  },
+  {
+    "revision": "44965deb7e5170fe060a",
+    "url": "/static/js/2.f21ad8e3.chunk.js"
+  },
+  {
+    "revision": "ccea82dbe1895660b41b0798f3cc5029",
+    "url": "/static/js/2.f21ad8e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "086cfc8a2be0a85360d4",
+    "url": "/static/js/main.a3c10f7d.chunk.js"
+  },
+  {
+    "revision": "f83962bd8d1cb382bd70",
+    "url": "/static/js/runtime-main.5c6948f4.js"
+  },
+  {
+    "revision": "3be357695d6d0d74bf483782a0e51671",
+    "url": "/static/media/aeonik-medium-webfont.3be35769.woff2"
+  },
+  {
+    "revision": "dca50ed394fbb2782adfe20483f6a86c",
+    "url": "/static/media/aeonik-medium-webfont.dca50ed3.woff"
+  }
+]);
